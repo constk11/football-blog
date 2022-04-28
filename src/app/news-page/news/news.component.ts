68a08530adf2020d84b068a3d46edc8967282d41
Konstantin Kudinov <const_k11@mail.ru>
2022-04-28 10:32:09 +0300

    added news page

--- src/app/news-page/news/news.component.ts
+++ src/app/news-page/news/news.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NewsService } from 'src/app/services/news.service';
+import { imagesTypes } from 'src/app/shared/images-types';
 import { News } from 'src/app/shared/interfaces';
+import { ImageService } from 'src/app/shared/services/image.service';
+
 
 @Component({
   selector: 'app-news',
@@ -12,7 +15,7 @@ export class NewsComponent implements OnInit {
 
   public news: News
 
-  constructor(private route: ActivatedRoute, private newsService: NewsService) { }
+  constructor(private route: ActivatedRoute, private newsService: NewsService, private imgService: ImageService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -20,4 +23,7 @@ export class NewsComponent implements OnInit {
     })
   }
 
+  public getImageSrc(pictureId: string): string {
+    return this.imgService.getImageSrc(pictureId, imagesTypes.newsPicture);
+  }
 }
